7059941c94b8432a1c518aa90a464d25eef881f8
mzparulina <pauline.revilla@gmail.com>
2023-01-24 13:26:15 -0500

    Add fetchCoordsByIP function

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
+
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -7,4 +9,14 @@ fetchMyIP((error, ip) => {
   }
 
   console.log('It worked! Returned IP:' , ip);
+  return ip;
+});
+
+fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned coordinates:' , coordinates);
 });
--- iss.js
+++ iss.js
@@ -22,5 +22,26 @@ const fetchMyIP = function(callback) {
   });
 };
 
+const fetchCoordsByIP = function(ip, callback) {
+  request(`http://ipwho.is/${ip}`, (error, response, body) => {
 
-module.exports = { fetchMyIP };
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    const parsedBody = JSON.parse(body);
+
+    if (!parsedBody.success) {
+      const message = `Success status was ${parsedBody.success}. Server message says: ${parsedBody.message} when fetching for IP ${parsedBody.ip}`;
+      callback(Error(message), null);
+      return;
+    } 
+
+    const { latitude, longitude } = parsedBody;
+
+    callback(null, {latitude, longitude});
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
